ba897b7ac886f2e8c96ac2623104f898b0c28141
Le Wang <uvdmw@studen.kit.edu>
2015-04-24 20:05:57 +0200

    Create folder "target/testDate" Get current time in formate HHmmss_SSS Save each pictures after each tests

--- jmjrst/src/test/java/org/jis/generator/RotateImageTest.java
+++ jmjrst/src/test/java/org/jis/generator/RotateImageTest.java
@@ -6,9 +6,13 @@ import javax.imageio.ImageIO;
 
 import java.awt.image.BufferedImage;
 import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import junit.framework.Assert;
 
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -17,11 +21,16 @@ public class RotateImageTest {
 	private Generator generator;
 	private File file = new File("src/test/resources/picture.jpg");
 	private BufferedImage bi;
+	private BufferedImage rotatedBi;
+	private int picWidth;
+	private int picHeight;
 
 	@Before
 	public void setUp() throws Exception {
 		generator = new Generator(null, 0);
 		bi = ImageIO.read(file);
+		picWidth = bi.getWidth();
+		picHeight = bi.getHeight();
 	}
 
 	/**
@@ -32,7 +41,8 @@ public class RotateImageTest {
 		/*
 		 *  eine beliebige Instanz von BufferedImage 
 		 */
-		assertEquals(bi, generator.rotateImage(bi, 0.0));
+		rotatedBi = generator.rotateImage(bi, 0.0);
+		assertEquals(bi, rotatedBi);
 	}
 	
 	/**
@@ -55,19 +65,17 @@ public class RotateImageTest {
 	 * Aufgabe 2.g, exchange width and height in 90 Grad and 270 Grad
 	 */
 	@Test
-	public void testExchangeWidthAndHeight() {
-		int picWidth = bi.getWidth();
-		int picHeight = bi.getHeight();
-		BufferedImage rotatedBi;
-		System.out.println(picWidth + " " + picHeight);
-		
+	public void testRotate90Degree() {
 		/*
 		 * 90 Grad
 		 */
 		rotatedBi = generator.rotateImage(bi, (Math.PI / 2));
 		assertEquals(picWidth, rotatedBi.getHeight());
 		assertEquals(picHeight, rotatedBi.getWidth());
-		
+	}
+	
+	@Test
+	public void testRotate270Degree() {
 		/*
 		 * 270 Grad
 		 */
@@ -75,5 +83,30 @@ public class RotateImageTest {
 		assertEquals(picWidth, rotatedBi.getHeight());
 		assertEquals(picHeight, rotatedBi.getWidth());
 	}
+	
+	@After
+	public void tearDown() {
+		SimpleDateFormat timestampFormat= new SimpleDateFormat("HHmmss_SSS");
+		Date timestamp = new Date(System.currentTimeMillis());
+		String timestampStr = timestampFormat.format(timestamp);
+		
+		String fileName = "rotatedPicture_" + timestampStr + ".jpg";
+		String path = "target/testDate/";
+		File folder = new File(path);
+		File output = new File(path + fileName);
+		try {
+			if (!folder.exists()) {
+				folder.mkdirs();
+			}
+			if (rotatedBi == null) {
+				ImageIO.write(bi, "jpg", output);
+			} else {
+				ImageIO.write(rotatedBi, "jpg", output);
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
 
 }
